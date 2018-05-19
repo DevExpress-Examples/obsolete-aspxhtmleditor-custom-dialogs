d731ed7c30af253ec44876a769dee11315c1f750
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/WebSite/DevExpress/ASPxSpellCheckerForms/SpellCheckOptionsForm.ascx.cs
@@ -0,0 +1,63 @@
+using DevExpress.Web.ASPxSpellChecker;
+using DevExpress.Web.ASPxSpellChecker.Localization;
+using DevExpress.Web.ASPxEditors;
+
+public partial class SpellCheckOptionsForm : SpellCheckerUserControl {
+    protected override void PrepareChildControls() {
+        PrepareFormCheckBoxes();
+        PrepareLanguagesCombo();
+        Localize();
+
+        base.PrepareChildControls();
+    }
+
+    protected override ASPxEditBase[] GetChildDxEdits() {
+        return new ASPxEditBase[] { chkbEmails, 
+                                    chkbMixedCase, 
+                                    chkbNumbers, 
+                                    chkbUpperCase, 
+                                    chkbUrls, 
+                                    comboLanguage, 
+                                    chkbTags 
+        };
+    }
+
+    protected override ASPxButton[] GetChildDxButtons() {
+        return new ASPxButton[] {
+            btnCancel,
+            btnOK
+        };
+    }
+
+    protected void PrepareFormCheckBoxes() {
+        ASPxSpellChecker spellChecker = ((ASPxSpellChecker)NamingContainer);
+        chkbEmails.Checked = spellChecker.SettingsSpelling.IgnoreEmails;
+        chkbMixedCase.Checked = spellChecker.SettingsSpelling.IgnoreMixedCaseWords;
+        chkbNumbers.Checked = spellChecker.SettingsSpelling.IgnoreWordsWithNumber;
+        chkbUpperCase.Checked = spellChecker.SettingsSpelling.IgnoreUpperCaseWords;
+        chkbUrls.Checked = spellChecker.SettingsSpelling.IgnoreUrls;
+        chkbTags.Checked = spellChecker.SettingsSpelling.IgnoreMarkupTags;
+    }
+    protected void PrepareLanguagesCombo() {
+        comboLanguage.Items.Clear();
+        for (int i = 0; i < SpellChecker.Dictionaries.Count; i++) {
+            if (comboLanguage.Items.FindByText(SpellChecker.Dictionaries[i].GetCulture().DisplayName) == null)
+                comboLanguage.Items.Add(SpellChecker.Dictionaries[i].GetCulture().DisplayName);
+        }
+        comboLanguage.SelectedItem = comboLanguage.Items.FindByText(SpellChecker.GetCulture().DisplayName);
+    }
+	protected void Localize() {
+		pnlOptions.HeaderText = ASPxSpellCheckerLocalizer.GetString(ASPxSpellCheckerStringId.GeneralOptions);
+		chkbUpperCase.Text = ASPxSpellCheckerLocalizer.GetString(ASPxSpellCheckerStringId.IgnoreUppercase);
+        chkbMixedCase.Text = ASPxSpellCheckerLocalizer.GetString(ASPxSpellCheckerStringId.IgnoreMixedCase);
+        chkbNumbers.Text = ASPxSpellCheckerLocalizer.GetString(ASPxSpellCheckerStringId.IgnoreNumbers);
+		chkbEmails.Text = ASPxSpellCheckerLocalizer.GetString(ASPxSpellCheckerStringId.IgnoreEmails);
+        chkbUrls.Text = ASPxSpellCheckerLocalizer.GetString(ASPxSpellCheckerStringId.IgnoreUrls);
+		chkbTags.Text = ASPxSpellCheckerLocalizer.GetString(ASPxSpellCheckerStringId.IgnoreTags);
+		pnlLanguageSelection.HeaderText = ASPxSpellCheckerLocalizer.GetString(ASPxSpellCheckerStringId.InternationalDictionaries);
+        lblChooseDictionary.Text = ASPxSpellCheckerLocalizer.GetString(ASPxSpellCheckerStringId.ChooseDictionary);
+        lblLanguage.Text = ASPxSpellCheckerLocalizer.GetString(ASPxSpellCheckerStringId.Language);
+		btnOK.Text = ASPxSpellCheckerLocalizer.GetString(ASPxSpellCheckerStringId.OkButton);
+		btnCancel.Text = ASPxSpellCheckerLocalizer.GetString(ASPxSpellCheckerStringId.CancelButton);
+	}
+}
---
+++ VB/WebSite/DevExpress/ASPxHtmlEditorForms/InsertLinkForm.ascx.vb
@@ -0,0 +1,75 @@
+﻿Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Collections
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web.ASPxHtmlEditor
+Imports DevExpress.Web.ASPxHtmlEditor.Localization
+Imports System.IO
+Imports DevExpress.Web.ASPxEditors
+
+Partial Public Class InsertLinkForm
+    Inherits HtmlEditorUserControl
+
+    Protected Overrides Sub PrepareChildControls()
+        Localize()
+
+        MyBase.PrepareChildControls()
+    End Sub
+    Private Sub Localize()
+        rblLinkType.Items.Add(ASPxHtmlEditorLocalizer.GetString(ASPxHtmlEditorStringId.InsertLink_Url), "URL")
+        rblLinkType.Items.Add(ASPxHtmlEditorLocalizer.GetString(ASPxHtmlEditorStringId.InsertLink_Email), "Email")
+        lblUrl.Text = ASPxHtmlEditorLocalizer.GetString(ASPxHtmlEditorStringId.InsertLink_Url) & ":"
+        txbURL.ValidationSettings.RequiredField.ErrorText = ASPxHtmlEditorLocalizer.GetString(ASPxHtmlEditorStringId.RequiredFieldError)
+        lblEmailTo.Text = ASPxHtmlEditorLocalizer.GetString(ASPxHtmlEditorStringId.InsertLink_EmailTo) & ":"
+        txbEmailTo.ValidationSettings.RequiredField.ErrorText = ASPxHtmlEditorLocalizer.GetString(ASPxHtmlEditorStringId.RequiredFieldError)
+        lblSubject.Text = ASPxHtmlEditorLocalizer.GetString(ASPxHtmlEditorStringId.InsertLink_Subject) & ":"
+        lblLinkDisplay.Text = ASPxHtmlEditorLocalizer.GetString(ASPxHtmlEditorStringId.InsertLink_DisplayProperties)
+        lblText.Text = ASPxHtmlEditorLocalizer.GetString(ASPxHtmlEditorStringId.InsertLink_Text) & ":"
+        lblToolTip.Text = ASPxHtmlEditorLocalizer.GetString(ASPxHtmlEditorStringId.InsertLink_ToolTip) & ":"
+        ckbOpenInNewWindow.Text = ASPxHtmlEditorLocalizer.GetString(ASPxHtmlEditorStringId.InsertLink_OpenInNewWindow)
+        btnOk.Text = ASPxHtmlEditorLocalizer.GetString(ASPxHtmlEditorStringId.ButtonOk)
+        btnChange.Text = ASPxHtmlEditorLocalizer.GetString(ASPxHtmlEditorStringId.ButtonChange)
+        btnCancel.Text = ASPxHtmlEditorLocalizer.GetString(ASPxHtmlEditorStringId.ButtonCancel)
+    End Sub
+
+    Protected Overrides Function GetChildDxEdits() As ASPxEditBase()
+        Return New ASPxEditBase() { rblLinkType, lblUrl, txbURL, lblEmailTo, txbEmailTo, lblSubject, txbSubject, lblLinkDisplay, lblText, txbText, lblToolTip, txbToolTip, ckbOpenInNewWindow }
+    End Function
+    Protected Overrides Function GetChildDxButtons() As ASPxButton()
+        Return New ASPxButton() { btnOk, btnCancel, btnChange }
+    End Function
+    Protected Overrides Function GetChildDxHtmlEditorRoundPanel() As ASPxHtmlEditorRoundPanel
+        Return rpInsertLink
+    End Function
+    Protected Function SaveUploadFile() As String
+        Dim fileName As String = ""
+        If HasFile() Then
+            Dim uploadFolder As String = HtmlEditor.SettingsImageUpload.UploadImageFolder
+            fileName = MapPath(uploadFolder) & uplImage.UploadedFiles(0).FileName
+            Try
+                uplImage.UploadedFiles(0).SaveAs(fileName, False)
+            Catch e1 As IOException
+                fileName = MapPath(uploadFolder) & uplImage.GetRandomFileName()
+                uplImage.UploadedFiles(0).SaveAs(fileName)
+            End Try
+        End If
+        Return Path.GetFileName(fileName)
+    End Function
+    Protected Function HasFile() As Boolean
+        Return uplImage.UploadedFiles IsNot Nothing AndAlso uplImage.UploadedFiles.Length > 0 AndAlso uplImage.UploadedFiles(0).FileName <> ""
+    End Function
+    Protected Sub uplImage_FileUploadComplete(ByVal sender As Object, ByVal args As DevExpress.Web.ASPxUploadControl.FileUploadCompleteEventArgs)
+        Try
+            args.CallbackData = SaveUploadFile()
+        Catch e As Exception
+            args.IsValid = False
+            args.ErrorText = e.Message
+        End Try
+    End Sub
+End Class
